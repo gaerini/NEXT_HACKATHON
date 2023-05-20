bd6e259dca26ac2e31a5a6db81e9ac29ccb82e38
jiho Kim <jimmy7335@gmail.com>
2023-05-20 16:00:36 +0900

    model

--- daedongjae/daedongapp/admin.py
+++ daedongjae/daedongapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Market, Group, Game, GameRoom
 # Register your models here.
+
+admin.site.register(Market)
+admin.site.register(Group)
+admin.site.register(Game)
+admin.site.register(GameRoom)
